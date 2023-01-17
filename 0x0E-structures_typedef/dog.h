c58b897df5858ed7e6e99798e97b2a24b040d8d0
b1codex <mabayojedamilola85@gmail.com>
2023-01-17 04:05:54 -0800

    ♡

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,31 +1,27 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
-* struct dog - a dog's basic info
-* @name: First member
-* @age: Second member
-* @owner: Third member
-*
-* Description: Longer description
-*/
-struct dog
+ * struct dog - a new type
+ *
+ * @name: the name of the dog (Django)
+ * @age: the age of the dog (3.5)
+ * @owner: the name of the owner (Jay)
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
-* dog_t - typedef for struct dog
-*/
+}
+	dog;
+/*task three, new type dog_t as new name of strcut dog */
 typedef struct dog dog_t;
 
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
-#endif
+#endif /* DOG */
